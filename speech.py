77763db01d558a303be9baa4b50359567ae5c7e5
Oscar Hermosa <ohermosa@gmail.com>
2018-11-22 22:19:48 +0100

    speech.py: less arguments and refactor random arg, datiops_bot: authorize new users and random

--- speech.py
+++ speech.py
@@ -21,12 +21,9 @@ def get_topics(path):
 
 
 def get_random_sentence(topic):
-  with open(topic) as file:
+  with open(dir + '/topics/' + topic) as file:
     sentences = [line for line in file]
-  audio_files = []
-  for sentence in sentences:
-    audio_files.append(get_text_to_speech_file(sentence))
-  return random.choice(audio_files)
+  return random.choice(sentences)
 
 
 def delete_cache():
@@ -34,7 +31,6 @@ def delete_cache():
     for file in files:
       if file.endswith(".mp3"):
         os.remove("/tmp/" + file)
-
 
 
 def get_text_to_speech_file(text):
@@ -46,65 +42,50 @@ def get_text_to_speech_file(text):
   return tmp_file_path
 
 
-def reproduce_file(_file):
-  subprocess.call(["cvlc", "--play-and-exit", _file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def reproduce_file(file):
+  subprocess.call(["cvlc", "--play-and-exit", file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 def play_message(text):
   reproduce_file(get_text_to_speech_file(text))
 
 
+def play_random(topic):
+  filename = topic
+  if filename in topics:
+    reproduce_file(get_text_to_speech_file(get_random_sentence(filename)))
+  else:
+    print("Topic doesn't exist")
+
+
 if __name__ == "__main__":
+  myself = os.path.abspath(sys.argv[0])
+  dir = os.path.dirname(myself)
+  topics = get_topics(dir + '/topics')
+  default_topic = random.choice(topics)
+
   parser = argparse.ArgumentParser()
   parser.add_argument("-t", "--text", type=str, help="Text to reproduce")
-  parser.add_argument("-r", "--random", action="store_true", help="Reproduce random sentence from files in ./topics")
-  parser.add_argument("-w", "--what", type=str, help="Specify topic to reproduce random sentence")
-  parser.add_argument("-g", "--generate", action="store_true", help="Generate mp3 files for precharged sentences")
+  parser.add_argument("-r", "--random", type=str, nargs="?", const=default_topic, help="Reproduce random sentence from ./topics/RANDOM. Without argument, reproduce random sentence from random topic")
   parser.add_argument("-l", "--list", action="store_true", help="List available topics")
   parser.add_argument("-c", "--clean_cache", action="store_true", help="Clean mp3 files in /tmp")
   args = parser.parse_args()
   args = vars(args) 
 
-  myself = os.path.abspath(sys.argv[0])
-  dir = os.path.dirname(myself)
 
   if args["text"]:
     text = args["text"]
     play_message(text)
     exit
 
-  topics = get_topics(dir + '/topics')
-
   if args["random"]:
-    if args["what"]:
-      what = args["what"]
-      exist = False
-      for item in topics:
-        #print(item)
-        if what in item:
-          audio_file = get_random_sentence(dir + '/topics/' + item)
-          exist = True
-      if not exist:
-        print ("Choosen topic doesn't exist")
-        quit()
-    else:
-      audio_file = get_random_sentence(dir + '/topics/' + random.choice(topics))
-    reproduce_file(audio_file)
+    topic = args["random"]
+    play_random(topic)
     exit
-
-  if args["generate"]:
-    for topic in topics:
-      with open(dir + '/topics/' + random.choice(topics)) as file:
-        sentences = [line for line in file]
-      for sentence in sentences:
-        generated_file = get_text_to_speech_file(sentence)
-        print ("Creating file %s..." %generated_file)
-    exit
-
+    
   if args["list"]:
     print ("This is the list of available topics:")
-    for item in topics:
-      print (item[:-4])
+    print(topics)
     exit
 
   if args["clean_cache"]:
